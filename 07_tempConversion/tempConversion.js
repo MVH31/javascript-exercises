40e735bbcd1f9521ee62feb1f09e7f230d394367
MVH <harshamv31@gmail.com>
2024-10-16 01:04:59 +0530

    tried seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let res = 0.0;
+
+  res = (temp - 32) * (5 / 9);
+
+  return parseFloat(res.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let res = 0.0;
+
+  res = (temp * 9 / 5) + 32;
+
+  return parseFloat(res.toFixed(1));
 };
 
 // Do not edit below this line

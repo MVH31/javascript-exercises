af0d7dc7cd4914bd25f6d90b1b89a12b8afc470f
MVH <harshamv31@gmail.com>
2024-10-16 00:55:29 +0530

    tried fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = 0;
+    if(!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) return "ERROR";
 
+    if(x > y) { t = x; x = y; y = t;}
+
+    for(let i = x; i <= y; i++) sum += i;
+
+    return sum;
 };
 
 // Do not edit below this line

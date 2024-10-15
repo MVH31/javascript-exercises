2f08556da9e8f57115d5bcc49674f1a06a733f51
MVH <harshamv31@gmail.com>
2024-10-16 00:58:18 +0530

    tried sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
+const leapYears = function(yyyy) {
+    if((yyyy % 4 == 0 && yyyy % 100 != 0) || yyyy % 400 == 0) return true;
 
+    return false;
 };
 
 // Do not edit below this line

070d239a2509299cad11d35ecad22058dbf5e3e7
MVH <harshamv31@gmail.com>
2024-10-15 23:59:31 +0530

    tried second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
+const repeatString = function(string, x) {
+    if(x < 0) return 'ERROR';
+    let str = "";
+    for(let i = 1; i <= x; i++) str += string;
 
+    return str;
 };
 
 // Do not edit below this line
